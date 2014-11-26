1945d10ee46b806120dae866904cde6e34446548
aldeano19 <erieljr1@gmail.com>
2014-11-26 10:31:57 -0500

    all info

--- TEST_ups_shipping_info_scrape.rb
+++ TEST_ups_shipping_info_scrape.rb
@@ -2,12 +2,16 @@ class TestUpsInfo
   def test_output()
     
     @a_hash = {
-      date_delivery:  'Friday, 10/31/2014',
-      time_delivery:  '11:28 A.M.',
-      left_at:        'Dock',
-      address:        'BELLEVUE, WA, US',
-      signed_by:      'MORGER',
-      signature_link: '<-- enter link of the picture -->'   #a link to img of signature in current machine
+         date_delivered: 'Friday, 10/31/2014',
+         time_delivered: '11:28 A.M.',
+                left_at: 'Dock',
+                address: '4455 148TH AVE NE BELLEVUE, WA, 98007, US',
+              signed_by: 'MORGER',
+      multiple_packages: '2',
+              billed_on: '10/24/2014',
+                   type: 'Package',
+                 weight: '6.00 lbs',
+          signature_pic: nil
     }
     
     return @a_hash
