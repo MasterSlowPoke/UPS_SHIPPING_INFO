cc276638d3cdde735cba08384208c24c7eb99a13
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-12-02 10:41:00 -0500

    updated to newest version

--- ups_shipping_info_scrape.rb
+++ ups_shipping_info_scrape.rb
@@ -1,107 +1,63 @@
-require 'selenium-webdriver'
-require 'date'
+require 'mechanize'
 
-class UpsInfo
-require 'securerandom'
+class ProofOfDelivery
+  attr_reader :result
 
-  def initialize(username, password, carrier)
-    @br = Selenium::WebDriver.for :chrome
-    @br.get 'https://www.ups.com/one-to-one/login?loc=en_US&returnto=https%3A%2F%2Fwwwapps.ups.com%2FWebTracking%2FreturnToDetails%3Floc%3Den_US&mcp=true'
-    
-    @br.find_element(:id, 'repl_id1').send_key(username)
-    @br.find_element(:id, 'repl_id4').send_key(password)
-    @br.find_element(:id, 'submitBtn').click()
-    @carrier = carrier
+  def initialize(username, password, tracking_number, shipper)
+    mech = Mechanize.new
+    page = log_into_ups_tracking_page(mech, username, password, tracking_number)
+
+    #retrieve information from UPS Page
+    parsed_page = Nokogiri::HTML page.body
+    xpaths = UPS_XPATHS
+
+    @result = {
+      shipper: shipper,
+      date_delivered: sanitize_scraped_text(parsed_page.xpath(xpaths[0]).children[3]),
+      left_at:  sanitize_scraped_text(parsed_page.xpath(xpaths[1]).children[3]),
+      signed_by: sanitize_scraped_text(parsed_page.xpath(xpaths[2]).children[3]),
+      address: (sanitize_scraped_text(parsed_page.xpath(xpaths[3]).children[3]) + " " + sanitize_scraped_text(parsed_page.xpath(xpaths[3]).children[5])).gsub(/\s+/, " ").squeeze,
+      multiple_packages: sanitize_scraped_text(parsed_page.xpath(xpaths[4]).children[0]).to_i,
+      billed_on: sanitize_scraped_text(parsed_page.xpath(xpaths[5]).children[0]),
+      type: sanitize_scraped_text(parsed_page.xpath(xpaths[6]).children[0]),
+      weight: sanitize_scraped_text(parsed_page.xpath(xpaths[7]).children[0]),
+      image: ups_encoded_image(mech, parsed_page),
+    }
   end
 
-  def get_info(tracking_number)
-    @br.find_element(:id, 'trackNums').send_key(tracking_number)
-    @br.find_element(:name, 'track.x').click()
+  def log_into_ups_tracking_page(mech, username, password, tracking_number)
+    page = mech.get 'https://www.ups.com/one-to-one/login?loc=en_US&returnto=https%3A%2F%2Fwwwapps.ups.com%2FWebTracking%2FreturnToDetails%3Floc%3Den_US&mcp=true'
     
-    wait = Selenium::WebDriver::Wait.new(:timeout => 10) 
+    #login to UPS
+    form = page.forms[1]
+    form.uid = username
+    form.password = password
+    page = form.submit form.buttons.first
 
-    xpaths = ['//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/dl[1]',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/dl[2]',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/dl[3]',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[2]/div[2]/fieldset/div[2]/dl/dd',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[1]',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[2]',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[3]',
-              '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[4]']
-    
-    wait.until {@br.find_element(:xpath, xpaths[7])}
-    
-    basic_information = []
-    xpaths.each do |path|
-      basic_information << @br.find_element(:xpath, path)
-    end
-    
-    hash_info = {}
-    
-    
-    date = basic_information[0].text.partition(":\n").last.split(' ')[1]
-    
-    if basic_information[0].text.partition(":\n").last.split(' ')[4] == 'A.M.'
-      time = basic_information[0].text.partition(":\n").last.split(' ')[3]
-    else
-      time =  (basic_information[0].text.partition(":\n").last.split(' ')[3].split(':')[0].to_i + 12).to_s + 
-               basic_information[0].text.partition(":\n").last.split(' ')[3].split(':')[1]
-    end
-    
-    year  = date.split('/')[2].to_i
-    month = date.split('/')[0].to_i
-    day   = date.split('/')[1].to_i
-    hour  = time.split(':')[0].to_i
-    minute= time.split(':')[1].to_i
-    
-    delivery_date_time = DateTime.new(year, month, day, hour, minute)
-    
-    year = basic_information[5].text.split('/')[2].to_i
-    month= basic_information[5].text.split('/')[0].to_i
-    day  = basic_information[5].text.split('/')[1].to_i
-    
-    bill_date = DateTime.new(year, month, day)
-    
-    hash_info[:carrier]           = @carrier
-    hash_info[:date_time_deliv]   = delivery_date_time                                               #DateTime object
-    hash_info[:left_at]           = basic_information[1].text.partition(":\n").last
-    hash_info[:signed_by]         = basic_information[2].text.partition(":\n").last
-    hash_info[:address]           = basic_information[3].text.partition(":\n").last
-    hash_info[:multiple_packages] = basic_information[4].text.to_i
-    hash_info[:billed_on]         = bill_date
-    hash_info[:type]              = basic_information[6].text
-    hash_info[:weight]            = basic_information[7].text
-    
-    pic_name = hash_info[:signed_by]
-    hash_info[:signature_pic]     = "signatures/#{pic_name}-#{SecureRandom.uuid}.png"
-    
-    image = @br.find_element(:xpath, '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/img')
-    link = image.attribute("src")
-    @br.get(link)
-    
-    begin
-      File.new(hash_info[:signature_pic], "w")
-      @br.save_screenshot(hash_info[:signature_pic])
-    rescue Exception => e
-      puts Dir.pwd
-      puts e.message
-    end
-    
-    return hash_info
+    #enter UPS tracking number
+    form = page.forms[2]
+    form.trackNums = tracking_number
+    form.submit form.buttons.first
   end
-    
-  def quit_browser()
-    @br.quit
+
+  def ups_encoded_image(mech, parsed_page)
+    img_tag = parsed_page.xpath('//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/img')
+    p "\n****\n#{img_tag.class}\n***\n"
+    return nil if img_tag.count == 0 || img_tag.attr('src').nil?
+    img_file = mech.get img_tag.attr('src').value
+    "data:image/png;base64,#{Base64.encode64 img_file.body}"
   end
+
+  def sanitize_scraped_text(data)
+    data.nil? ? "" : data.text.strip
+  end
+
+  UPS_XPATHS = ['//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/dl[1]',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/dl[2]',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[1]/fieldset/div/fieldset/div/dl[3]',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[2]/div[2]/fieldset/div[2]/dl/dd',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[1]',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[2]',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[3]',
+                '//*[@id="fontControl"]/fieldset/div[3]/fieldset/div/fieldset/div/fieldset/div[1]/div[3]/fieldset/div[2]/dl/dd[4]']
 end
-
-# username = 'pinchpro'
-# password = 'Pinchme123'
-# tracking = '1Z5560TT0393090598'
-#
-# test = UpsInfo.new(username, password, 'UPS')
-# info = test.get_info(tracking)
-#
-# puts info[:carrier]
-#
-# test.quit_browser()
